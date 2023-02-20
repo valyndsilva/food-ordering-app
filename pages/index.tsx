f63a9b91cb1ec591ad7439fc05e929d881df7a07
valyndsilva <valyndsilva@gmail.com>
2023-02-20 09:42:33 +0000

    fix mondoDB fetch issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { About, Featured, PizzaList } from "../components";
+import Product from "../models/Product";
 import { setIsAuthenticated } from "../redux/slices/authSlice";
+import dbConnect from "../util/mongodb";
 interface Props {
   products: any;
   isLoggedIn: any;
 }
 const Home = ({ products, isLoggedIn }: Props) => {
-  console.log({ isLoggedIn });
+  // console.log({ isLoggedIn });
   const dispatch = useDispatch();
   dispatch(setIsAuthenticated(isLoggedIn));
   return (
@@ -32,11 +34,20 @@ export const getServerSideProps = async (context: any) => {
     isLoggedIn = true;
   }
 
-  const productRes = await axios.get("http://localhost:3000/api/products");
+  await dbConnect();
+  /* Fetch existing data from mongoDB*/
+  const result = await Product.find({});
+  const products = result.reverse().map((doc) => {
+    const product = doc.toObject();
+    product._id = product._id.toString();
+    return product;
+  });
+  // const productRes = await axios.get("http://localhost:3000/api/products");
 
   return {
     props: {
-      products: productRes.data,
+      // products: productRes.data,
+      products: JSON.parse(JSON.stringify(products)),
       isLoggedIn: isLoggedIn,
     },
   };
